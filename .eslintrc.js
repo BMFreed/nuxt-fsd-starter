ab2aa7612e70f054dcab7634ea95514b9bfa55ae
Богдан Биницкий <bbinitskiy@ibs.ru>
2024-08-29 19:34:04 +0300

    refactor(style): Обновить конфигурацию линтера

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,52 @@
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
   extends: [
-    './eslint/baseConfig',
-    './eslint/featureSlicedConfig',
-    './eslint/rulesets/vueConfig',
-    './eslint/rulesets/typescriptConfig',
-    './eslint/rulesets/jsdocConfig',
-    './eslint/overrides/configOverrides',
-    './eslint/rulesets/mainRules',
+    'plugin:nuxt/recommended',
+    'hardcore',
+    'hardcore/ts',
+    'hardcore/vue',
+    'hardcore/feature-sliced',
+    'hardcore/jsdoc',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  overrides: [
+    {
+      files: ['.*rc.js', 'eslint/**/*.js', 'nuxt.config.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.configs.json',
+        tsconfigRootDir: __dirname,
+      },
+      rules: {
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@stylistic/lines-around-comment': 'off',
+        '@typescript-eslint/no-magic-numbers': 'off',
+        'no-magic-numbers': 'off',
+        'unicorn/prefer-module': 'off',
+        'import/no-commonjs': 'off',
+        'import/unambiguous': 'off',
+        'import/extensions': 'off',
+        'import/no-internal-modules': 'off',
+        'jsdoc/require-jsdoc': 'off',
+        'no-template-curly-in-string': 'off',
+      },
+    },
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        project: './tsconfig.json',
+      },
+    },
   ],
 }
